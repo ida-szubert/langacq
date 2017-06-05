101e4788ed161c88567b289da7a49c92fae1b8e9
Ida Szubert <katarzynaidaszubert@gmail.com>
2017-06-05 12:10:20 +0100

    removed unused modules, moved evaluation code to a subdirectory

--- bash_scripts/run_all_exps.sh
+++ bash_scripts/run_all_exps.sh
@@ -19,5 +19,11 @@ python -u SemLearn.py i_n - $distractors -i trainFiles/trainPairs.filtered.ready
 done
 
 
+#!/usr/bin/env bash
+
+#python2 SemLearn.py i_n -i trainFiles/Adam{}_lf.txt -t my_test/train_test_parses_terminal_reps1_ -n my_test/train_train_parses_terminal_reps1_ -s 20  --dump_out my_test/train_test_parses_terminal_reps1_dump_ --dinter 100 --dump_vr --analyze --dotest
+python2 SemLearn.py i_n -i trainFiles/trainPairs.filtered.ready_{} -t my_test/Eve_train_test_parses_terminal_reps1_ -n my_test/Eve_train_train_parses_terminal_reps1_ -s 21 -m my_test/Eve_model_throwaway.pkl --dump_out my_test/Eve_train_test_parses_terminal_reps1_dump_ --dinter 100 --dump_vr --analyze
+
+
 
 
--- templateReader.py
+++
@@ -1,8 +0,0 @@
-# this reads in the smeantic templates from a file
-
-class template:
-	def __init__(self,name,type,args):
-		self.name = name
-		self.type = type
-		for arg in self.args:
-			args.append(
